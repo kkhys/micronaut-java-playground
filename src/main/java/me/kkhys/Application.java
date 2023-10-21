85e23ab39e2c6dc8f361a684304075169dacbbd7
ktnkk <ktnkk@pm.me>
2023-10-21 19:42:59 +0900

    feat(*): initial commit

---
+++ src/main/java/me/kkhys/Application.java
@@ -0,0 +1,10 @@
+package me.kkhys;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
